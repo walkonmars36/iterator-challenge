74fe52a2988f9274f1f64ddb0f2c4c170f72f197
Mark Lawson <mark@walkonmars.co.uk>
2022-06-01 11:53:32 +0100

    working through challenges

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// 1
+
 /* Takes an array of names as a parameter.
 It needs return a new array with each of the first letters of the names capitalized */
 
@@ -5,6 +7,8 @@ const nameArray = ["charlie", "rob"];
 
 const capitaliseFirstLetter = (array) => {
   return array.map((name) => {
+    // console.log(name.charAt(0).toUpperCase());
+    // console.log(name.substring(1));
     return name.charAt(0).toUpperCase() + name.substring(1);
   });
 };
@@ -19,8 +23,9 @@ const mixedLetters = ["c", "r", "a"];
 
 const removeVowels = (array) => {
   const vowels = ["a", "e", "i", "o", "u"];
-  let consonants = array.filter((letter) => {
-    letter.includes(!vowels);
+
+  const consonants = array.filter((letter) => {
+    return vowels.indexOf(letter) === -1;
   });
 
   return consonants;
@@ -38,12 +43,48 @@ Total the numbers and divide by the amount of numbers in the array
 const scoresArray = [25, 50, 175, 50];
 
 const meanAverage = (array) => {
-  const total = array.reduce((prevVal, currentVal) => {
-    prevVal + currentVal;
-  });
-
-  console.log(total);
-  return total;
+  return array.reduce((prevVal, currentVal) => (prevVal += currentVal)) / array.length;
 };
 
-meanAverage(scoresArray);
+console.log(meanAverage(scoresArray));
+
+// 2
+
+/* Takes an array of words
+Returns a new array of words
+Figure out the difference with the examples below and use an array iterator method to implement it.*/
+
+const wordArraySpot = ["spray", "limit", "disco", "exuberant", "destruction", "present"];
+
+const sliceWords = (array) => {
+  return array.slice(-3);
+};
+
+console.log(sliceWords(wordArraySpot));
+
+/* Takes an array of words
+Returns a new array of words
+Figure out the difference with the examples below and use an array iterator method to implement it. */
+
+const discoArray = ["please camel", "join casing", "these disco", "with shoes"];
+
+const camelize = (array) => {};
+
+console.log(camelize(discoArray));
+
+/* Takes an array of scrabble tile objects
+Returns a score
+Figure out the difference with the examples below and use an array iterator method to implement it.
+*/
+
+const scrabbleArray = [
+  {letter: "a", value: 1},
+  {letter: "k", value: 5},
+  {letter: "c", value: 3},
+];
+
+const totalValues = (array) => {
+  return array.map((item) => item.value).reduce((prev, current) => (prev += current));
+};
+
+console.log(totalValues(scrabbleArray));
